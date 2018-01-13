520d8d5f458e62e0b332e6c723fc1b510a3c6874
masaki shoji <dex10619@gmail.com>
2018-01-13 17:57:53 +0900

    Add an UI test.

--- app/src/androidTest/kotlin/com/badlogic/masaki/passwordmanagementsample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/kotlin/com/badlogic/masaki/passwordmanagementsample/ExampleInstrumentedTest.kt
@@ -2,12 +2,20 @@ package com.badlogic.masaki.passwordmanagementsample
 
 import android.content.Context
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.matcher.ViewMatchers.withId
+import android.support.test.espresso.matcher.ViewMatchers.withText
+import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.badlogic.masaki.passwordmanagementsample.activity.LoginActivity
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,12 +24,28 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @JvmField
+    @Rule
+    val mActivityRule = ActivityTestRule(LoginActivity::class.java)
+
+    var mActivity: LoginActivity? = null
+
+    var mContext: Context? = null
+
+    @Before
+    fun setUp() {
+        mActivity = mActivityRule.activity
+        mContext = InstrumentationRegistry.getTargetContext()
+    }
+
     @Test
     @Throws(Exception::class)
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.badlogic.masaki.passwordmanagementsample", mContext?.packageName)
+    }
 
-        assertEquals("com.badlogic.masaki.passwordmanagementsample", appContext.packageName)
+    @Test
+    fun test() {
+        onView(withId(R.id.textView)).check(matches(withText(mContext?.getString(R.string.app_name))))
     }
 }
